972107a67fbdc52a60e5de98e53fd50365d40eec
basanirakeshreddy <rakeshbasani.qa@gmail.com>
2014-10-15 18:08:21 +0530

    upload management role

--- src/crm/Management.java
+++ src/crm/Management.java
@@ -1,6 +1,7 @@
 package crm;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -13,17 +14,22 @@ import testUtils.Helper;
 
 
 public class Management extends Helper {
+	
   @Test
   public void Login() throws Exception 
   {
+	 
 	  //////////// logging into the site///////////////////////////
-	  driver.get(config.getProperty("url"));
+
+	  driver.get(config.getProperty("Murl"));
 	  help.login("ajaytesting4@gmail.com", "password");
 	  
-	  //////////////// Expanding tree menu of proposals ////////////
-	  driver.findElement(By.cssSelector("span.symbol-close")).click();
+	  //////////////// Expanding tree in the left pane of page ////////////
+	  
+	  help.expand();
 	  
 	  /////////////// All Proposals ///////////////////////////////
+	 
 	  driver.findElement(By.id("proposalsList")).click();
 	  driver.findElement(By.tagName("input")).click();
 	  driver.findElement(By.tagName("input")).sendKeys("sreekar");
@@ -35,11 +41,44 @@ public class Management extends Helper {
 		  help.sleep(1);
 	  }
 	  help.sleep(2);
-	 driver.findElement(By.className("analyse")).click();
-	 help.sleep(2);
-	 help.screenshot("all proposals");
-
-      /////////////// All lost competition ///////////////////////////////
+	  List<WebElement> ls =driver.findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+	  System.out.println("No of rows " + ls.size());
+	  ArrayList<String> ar = new ArrayList<String>();
+	  for (int i=0;i<ls.size();i++)
+	  {
+		 List<WebElement> lss = ls.get(i).findElements(By.tagName("td"));
+		 System.out.println("No of columns " + lss.size());
+		 String s1= lss.get(0).getText();
+		 ar.add(s1);	
+		 String s2= lss.get(1).getText();
+		 ar.add(s2);
+	  }  
+	  System.out.println(ar);
+	  driver.findElement(By.className("analyse")).click();
+	  help.sleep(2);
+	  List<WebElement> ls2 =driver.findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+	  System.out.println("No of rows in track it lead details " + ls2.size());
+	  ArrayList<String> ar1 = new ArrayList<String>();
+	  List<WebElement> lss2 = ls2.get(0).findElements(By.tagName("td"));
+	  System.out.println("No of columns in track it lead details " + lss2.size());
+	  String s11= lss2.get(0).getText();
+	  ar1.add(s11);	
+	  String s21= lss2.get(1).getText();
+	  ar1.add(s21);
+	  System.out.println(ar1);
+	  if(ar1.get(0).contains(ar.get(0)))
+	  {
+		 if(ar1.get(1).contains(ar.get(1))){
+		 System.out.println("Data is matching exactly");
+	  }
+	  }else
+	  System.out.println("Data doesnt match");
+	 
+	 
+     
+     
+     
+     /////////////// All lost competition ///////////////////////////////
 	  driver.findElement(By.id("lostCompetitionList")).click();
 	  driver.findElement(By.tagName("input")).click();
 	  driver.findElement(By.tagName("input")).sendKeys("sreekar");
@@ -52,9 +91,6 @@ public class Management extends Helper {
 		  }
       //driver.findElement(By.className("analyse")).click();
 	  
-	  //////////////// Expanding tree menu of customers ////////////
-	  driver.findElement(By.cssSelector("span.symbol-close")).click();
-	  
 	  /////////////// All customers //////////////////////////
 	  driver.findElement(By.id("customersList")).click();
 	  driver.findElement(By.tagName("input")).click();
@@ -66,12 +102,11 @@ public class Management extends Helper {
 		  ele2.get(j).click();
 		  help.sleep(1);
 		  }
+	 
 	  driver.findElement(By.className("analyse")).click();
-	  help.sleep(2);
+	  help.screenshot("all proposals");
 	  help.screenshot("all customers"); 
-      ////////////////Expanding tree menu of Quotes ////////////
-	  driver.findElement(By.cssSelector("span.symbol-close")).click();
-
+     
       /////////////// All Quotes //////////////////////////
 	  driver.findElement(By.id("quotesList")).click();
       driver.findElement(By.tagName("input")).click();
@@ -85,18 +120,15 @@ public class Management extends Helper {
       }
     // driver.findElement(By.className("analyse")).click();
      
-     /////////////////// Expanding tree menu of Lead search /////////////////////////////
-     driver.findElement(By.cssSelector("span.symbol-close")).click();
-     
      ///////////////////////// Search leads //////////////////////////////////////////////
      driver.findElement(By.id("serachLeads123")).click();
      ArrayList<String> newTab = new ArrayList<String>(driver.getWindowHandles());
 	 driver.switchTo().window(newTab.get(1));
-	 List<WebElement> ls = driver.findElement(By.id("fields_to_get")).findElements(By.tagName("input"));
-	 System.out.println("Required field container size of lead search " + ls.size());
-	 for (int j=0;j<ls.size();j++)
+	 List<WebElement> ls1 = driver.findElement(By.id("fields_to_get")).findElements(By.tagName("input"));
+	 System.out.println("Required field container size of lead search " + ls1.size());
+	 for (int j=0;j<ls1.size();j++)
      {
-   	  ls.get(j).click();
+   	  ls1.get(j).click();
      }
 	 help.sleep(2);
      help.screenshot("search leads");
@@ -106,11 +138,9 @@ public class Management extends Helper {
      driver.close();
      ArrayList<String> newTab1 = new ArrayList<String>(driver.getWindowHandles());
 	 driver.switchTo().window(newTab1.get(0));
-	 
-     ///////////////////////// Expanding Tree menu of My Account /////////////////////
-     driver.findElement(By.cssSelector("span.last.symbol-close")).click();
-     
+	
     /////////////////////////// change password ////////////////////////////////////
+	 
      driver.findElement(By.xpath(".//*[@id='tree_menu']/li[5]/ul/li/a")).click();
      driver.findElement(By.id("oldPassword")).sendKeys("password");
      driver.findElement(By.id("newPassword")).sendKeys("rakesh");
@@ -130,6 +160,7 @@ public class Management extends Helper {
 
  @AfterMethod
   public void afterClass() {
+	
 	 driver.quit();
   }
 
