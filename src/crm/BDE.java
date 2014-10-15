bd087f464cda4c3d2d7ed32ff289f72fc98f51d7
vlakshmi90 <lakshmipriya.sure@gmail.com>
2014-10-15 19:21:35 +0530

    reuploading BDE file

--- src/crm/BDE.java
+++ src/crm/BDE.java
@@ -1,109 +1,117 @@
 package crm;
 
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import testUtils.Helper;
-import crm.BDEMethods;
 
 public class BDE extends Helper{
-	BDEMethods b = new BDEMethods();
+	String[] expected = {"Research Phase", "Work ", "Closed Phase", "Cold Storage", "Lead Search", "Lead Edit", "My Account", 
+			"Research On Company", "Work on Lead", "Today's FollowUp", "All FollowUps", "Lead Close", "Cold Storage", "Search Leads", "Edit Leads", "Change Password"};
+			
 	List<String> lisub = new ArrayList<String>();
-	
-	public void ExpandCollapse(List<WebElement> tree)
-	{
-		for (int i=0 ; i< tree.size(); i++)
-		{
-			lisub.add(tree.get(i).getText());
-			System.out.println("this upper" + lisub);
-			System.out.println(tree.get(i).getText());
-	   
-			List<WebElement> sub = tree.get(i).findElements(By.tagName("span"));
-			//sub.get(0).click();
-			if(sub.get(0).getAttribute("class").contains("open"))
-			{   
-				List<WebElement> li2= driver.findElement(By.id("tree_menu")).findElements(By.tagName("a"));
-				for (int j=0 ;j <li2.size();j++){
-					System.out.println(li2.get(j).getText());
-					lisub.add(li2.get(j).getText());
-				}
-			}	   
-		}
+	@BeforeTest
+	public void beforeMethod() throws Exception{
+		browser();
+		maxbrowser();
+		driver.get(config.getProperty("url"));
+		login("sreekar.jakkula@nexiilabs.com", "password");
+		browsererror();		
 	}
-    public void ResearchOnCompany(List<WebElement> tree)
-    {
-    	List<WebElement> sub = tree.get(0).findElements(By.tagName("span"));
-		   sub.get(0).click();
-		   
-		   if((sub.get(0).getAttribute("class").contains("open")))// && (tree.get(i).getText().equalsIgnoreCase("Research Phase")))
-		   {   
-			   System.out.println("this is Research Phase");
-			   
-			   List<WebElement> subtree = tree.get(0).findElements(By.tagName("a"));
-			   subtree.get(0).click();
+	/*@Test
+	public void ExpandCollapse()
+	{	
+		List<WebElement> tree = driver.findElement(By.id("tree_menu")).findElements(By.className("close"));
+		sleep(1);
+		for (int i = 0; i < tree.size(); i++)
+			lisub.add(tree.get(i).getText());	
+		expand();
+		List<WebElement> subtree= driver.findElement(By.id("tree_menu")).findElements(By.tagName("a"));
+		for (int i = 0; i < subtree.size(); i++)
+			lisub.add(subtree.get(i).getText());
+		collapse();			
+		System.out.println("+++++++++ This List of Links in tree and sub tree ++++++\n" + lisub);
+	    for ( int k=0 ; k< expected.length; k++)
+	    {
+	    	String list = lisub.get(k);
+	    	if (list.equals(expected[k]))
+	    		System.out.println("Passed on search of link of tree :" +list);
+	    	else
+	    		System.out.println("Failed on search of links of tree :" +list);
+		}
+	    
+	}*/
+	@Test
+	public void ResearchOnCompany()
+	{
+		expand();
+		List<WebElement> subtree= driver.findElement(By.id("tree_menu")).findElements(By.tagName("a"));
+		for (int i=0; i<subtree.size(); i++)
+			if((subtree.get(i).getText().equalsIgnoreCase("Research on company")))
+			{   	   
+			   subtree.get(i).click();
 			   System.out.println(" Navigate to 'Research On Company' page  ");
-			   help.sleep(2000);
+			   sleep(2);
 			   WebElement cont= driver.findElement(By.id("container"));
 			   if (cont.findElement(By.tagName("h1")).getText().equalsIgnoreCase("Lead Research"))
 			   {
-				   System.out.println("The Lead Research page is navigated sucessfully.");
+				   System.out.println("######### The Lead Research page is navigated sucessfully. ##########");
 				   
 				   List<WebElement> table = driver.findElement(By.id("example")).findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
 				   System.out.println(table.size() + " is " + table);
 				   WebElement res = table.get(0).findElement(By.className("segregate"));
 				   res.click();
-				   System.out.println("The Button clicked is:" +res.getText());
+				   //System.out.println("The Button clicked is:" +res.getText());
 				   WebElement cont1= driver.findElement(By.id("dialog-form"));
-				   System.out.println("cont1 size"+ cont1);
 				   System.out.println(cont1.findElement(By.tagName("h1")).getText());
 				   if (driver.findElement(By.tagName("h1")).getText().equalsIgnoreCase("Research on Lead"))
 				   {
-					  List<WebElement> lab1 = driver.findElement(By.name("companyfundstatus")).findElements(By.tagName("option"));
-					  lab1.get(0).click();
+					  WebElement seg = driver.findElement(By.id("segregatebutton"));
+					  seg.submit();
+					  List<WebElement> ermg =driver.findElement(By.id("result_msg_div")).findElements(By.tagName("label"));
+					  if(ermg.get(0).getText().equalsIgnoreCase("Please Select Company Fund Status"))
+						  System.out.println("Error message is displayed as expected");
+				      
+					  WebElement lab1 = driver.findElement(By.name("companyfundstatus"));
+					  lab1.click();
+					  sleep(1);
+					  List<WebElement> l1opt= lab1.findElements(By.tagName("option"));
+					  l1opt.get(random(l1opt.size())).click();
+					  sleep(1);
+					  
+					  WebElement lab2 = driver.findElement(By.name("companystatus"));
+					  lab2.click();
+					  sleep(1);
+					  List<WebElement> l2opt= lab1.findElements(By.tagName("option"));				  
+					  l2opt.get(random(l2opt.size())).click();
+					  sleep(1);
+					  
+					  WebElement cmt = driver.findElement(By.name("researchcomment"));
+					  cmt.sendKeys("This is completed as per the schedule.");
+					  
+					  
+					  
 				   }
 				   
 				   
 			   }
 		   }
-    
-			   
+ 
+	}		   
     }
+	
+			   
+    
 	
 	
 
-	@BeforeTest
-	public void beforeMethod(){
-		help.browser();
-	}
-	@Test
-	public void BDEModule() throws Exception 
-	{
-		//h.browser();
-		driver.get("http://192.168.50.32:8080/leadcrm");
-		help.login("sreekar.jakkula@nexiilabs.com", "password");
-		System.out.println(driver.getTitle());
-		driver.manage().window().maximize();	
-		
-		List<WebElement> tree = driver.findElement(By.id("tree_menu")).findElements(By.className("close"));
-		/*b.listmethodassert(tree);
-		ExpandCollapse(tree);
-		List<String> lisub = new ArrayList<String>();*/
-	    ResearchOnCompany(tree);
-		
 	
-  }
-}
+	
+
