fd3a47df0cb3c95b5d7f4c33fcf84e4e897b8868
unknown <Admin@tulasiramg.nexiilabs.net>
2014-10-15 19:14:57 -0700

    ReUploading Admin File

--- src/crm/AdminLogin.java
+++ src/crm/AdminLogin.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 
 import testUtils.Helper;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
@@ -20,65 +21,83 @@ import org.testng.annotations.Test;
 
 public class AdminLogin extends Helper{
 	WebDriver driver ;
-	@BeforeMethod
+	/*@BeforeMethod
 	public void bf(){
 		 //help.browser();
-		driver = new ChromeDriver();
-		 driver.get(config.getProperty("url"));
-	}
- /*@Test
+		driver = new FirefoxDriver();
+		 driver.get("http://192.168.50.32:8080/leadcrm/login.jsp");
+	}*/
+ @Test
   public void create() throws InterruptedException {
 	  driver = new FirefoxDriver();
 	  driver.get("http://192.168.50.32:8080/leadcrm/login.jsp");
-	  /*==========Login with Admin Credentials========
-	  help.login(config.getProperty("AdminuserName"), config.getPropert("AdminPass"));
-	  //driver.findElement(By.id("username")).sendKeys("srini.sanchana@gmail.com");
-	  //driver.findElement(By.id("password")).sendKeys("password");
-	  //driver.findElement(By.cssSelector("p.login.button")).submit();
+	  driver.manage().window().maximize();
+	  /*==========Login with Admin Credentials========*/
+	  driver.findElement(By.id("username")).sendKeys("srini.sanchana@gmail.com");
+	  driver.findElement(By.id("password")).sendKeys("password");
+	  driver.findElement(By.cssSelector("p.login.button")).submit();
 	  
-	  /*==========Side Tree Expansion==========
+	  /*==========Side Tree Expansion==========*/
 	  
 	  driver.findElement(By.cssSelector("span.symbol-close")).click();
 	  
-	  /*======Clicking on Create button=======
+	  /*======Clicking on Create button=======*/
 	  
 	   driver.findElement(By.id("createUser")).click();
 	   Thread.sleep(3000);
 	   
-	  /*======'Manager' Drop down button presence and its working===== 
+	  /*======'Manager' Drop down button presence and its working=====*/ 
 	  // driver.findElement(By.name("manager")).click();
 	   System.out.println("The drop down is present");
 	    List<WebElement> M = driver.findElement(By.name("manager")).findElements(By.tagName("option"));
 	  
 	  System.out.println(M.size());
 	  
-	  for(int i=0;i<M.size();i++){
+	// for(int i=0;i<M.size();i++){
 		  
-		  M.get(i).click();
-		  Thread.sleep(1000);
-	 } 
+	//  M.get(i).click();
+	//  Thread.sleep(1000);
+	 //} 
 	  
-	  /*======'Role' Drop down button presence and its working=====
+	  /*======'Role' Drop down button presence and its working=====*/
 	   //driver.findElement(By.name("role")).click();
 	  System.out.println("The drop down is present");
 	   List<WebElement> R = driver.findElement(By.name("role")).findElements(By.tagName("option"));
 	   System.out.println(R.size());
-	   for(int j=0;j<R.size();j++){
-		  R.get(j).click();
-		  Thread.sleep(1000);
-	  }													
+	 //  for(int j=0;j<R.size();j++){
+	//	  R.get(j).click();
+	//	  Thread.sleep(1000);
+	//  }													
+	  
+	   /*========= Creation of New User ======*/
+	   sh = w.getSheet(6);
+	   int columns = sh.getColumns();
+		int rows = sh.getRows();
+		String data;
+		int col;
+		for(int row=1;row<rows;row++){
+			 col=0;
+			 
+			 //data = sh.getCell(col, row).getContents();
+			 //System.out.println(data);
+			 System.out.println("*************************");	
+	   driver.findElement(By.name("firstname")).sendKeys(sh.getCell(col, row).getContents());
+	   sleep(1);
+	  
+	   driver.findElement(By.name("lastname")).sendKeys(sh.getCell(++col, row).getContents());
+	   sleep(1);
 	   
-	   /*========= Creation of New User ======
-	   driver.findElement(By.name("firstname")).sendKeys("Rakesh Reddy");
-	   driver.findElement(By.name("lastname")).sendKeys("basani");
-	   driver.findElement(By.name("email")).sendKeys("rakeshbasani@gmail.com");
-	   driver.findElement(By.name("empid")).sendKeys("Temp0115");
+	   driver.findElement(By.name("email")).sendKeys(sh.getCell(++col, row).getContents());
+	   sleep(1);
+	   driver.findElement(By.name("empid")).sendKeys(sh.getCell(++col, row).getContents());
+	   sleep(1);
 	   Random r = new Random();
 	   int a = r.nextInt(M.size());
 	   if (M.get(a).getText().contentEquals("--- SELECT ---")){
 		 a++;  
 	   }
 	   M.get(a).click();
+	   sleep(1);
 	   System.out.println(M.get(a).getText());
 	  Random r1 = new Random();
 	   int b = r1.nextInt(R.size());
@@ -89,7 +108,8 @@ public class AdminLogin extends Helper{
 	   System.out.println(R.get(b).getText());
 	   if(R.get(b).getText().equals("Architect")){
 	       driver.findElement(By.id("Service")).click();  
-	       driver.findElement(By.className("medium")).sendKeys("data base specialist");
+	       sleep(1);
+	       driver.findElement(By.className("medium")).sendKeys("database admin");
 	       sleep(2);
 	       driver.findElement(By.id("createbutton")).click(); 
 	       sleep(2);
@@ -104,34 +124,22 @@ public class AdminLogin extends Helper{
 		 int c = r.nextInt(ls.size());
 		 ls.get(c).click();
 	   }
-		 driver.findElement(By.id("registerbutton")).submit();   
-  	}			*/
-  @Test
+		 driver.findElement(By.id("registerbutton")).submit();  
+		 driver.findElement(By.name("firstname")).clear();
+		 driver.findElement(By.name("lastname")).clear();
+		 driver.findElement(By.name("email")).clear();
+		 driver.findElement(By.name("empid")).clear();
+		}
+ }
+ /*@Test
   public void update() throws Exception   {
-	 
-	  //driver = new FirefoxDriver();
-	  //driver.get("http://192.168.50.32:8080/leadcrm/login.jsp");
-	  
-	  
-	  /*==========Login with Admin Credentials========*/
-	  System.out.println(config.getProperty("AdminuserName"));
-	  sleep(5);
-	  help.login(config.getProperty("AdminuserName"), config.getProperty("AdminPass"));
-	// driver.findElement(By.id("username")).sendKeys("srini.sanchana@gmail.com");
-	// driver.findElement(By.id("password")).sendKeys("password");
-	 //driver.findElement(By.cssSelector("p.login.button")).submit();
-	  
-	  
-	  /*==========Side Tree Expansion==========*/
-	  sleep(2);
-	  driver.findElement(By.cssSelector("span.symbol-close")).click();
-	  
-	  /*======Clicking on Update button=======*/
+		   
+	  /*======Clicking on Update button======= 
 	  
 	   driver.findElement(By.id("updateUser")).click();
 	   //sleep(3);
 	   
-   /*====== Verifying the Show Entries Drop down ========    */
+   /*====== Verifying the Show Entries Drop down ========   
 	   List<WebElement> lw = driver.findElement(By.name("example_length")).findElements(By.tagName("option"));
 	   
 	   System.out.println(lw.size());
@@ -150,7 +158,7 @@ public class AdminLogin extends Helper{
 		 sleep(3);
 	   }
 	  
-	 /*======== Sorting of the diplayed values ========
+	 /*======== Sorting of the diplayed values ======== 
 	   //sleep(3);
 	  // driver.findElement(By.className("sorting_asc")).click();
 	   List<WebElement> tbody = driver.findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
@@ -193,19 +201,50 @@ public class AdminLogin extends Helper{
 		   }
 	   
 	   }
-	   System.out.println("The users are diplayed in the sorted order");    */
+	   System.out.println("The users are diplayed in the sorted order");    
 	   
-	    /*====== Searching for a specific user ========  */
+	    /*====== Searching for a specific user ========  
 	   driver.findElement(By.tagName("input")).sendKeys("tulasiram");           
 	   driver.findElement(By.id("118")).click();
+	   sleep(4);
 	   List<WebElement> man=driver.findElement(By.name("manager")).findElements(By.tagName("option"));
 	   System.out.println(man.size());
+	   Random rn= new Random(man.size());
+	   int rnm = rn.nextInt(man.size());
+	   man.get(rnm).click();
+	   
+	   
 	   driver.findElement(By.id("updatebutton")).submit();
 	   sleep(2);
-	   Actions ac = new Actions(driver);
-       ac.moveToElement(driver.findElement(By.cssSelector("button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-icon-only.ui-dialog-titlebar-close"))).build().perform();
-       driver.findElement(By.cssSelector("button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-icon-only.ui-dialog-titlebar-close")).click();
+	   WebElement we = driver.findElement(By.className("success_msg"));
+	   System.out.println(we.getText());
+	   if(we.getText().equalsIgnoreCase("Successfully Updated...")){
+		   System.out.println("user updated successfully");
 	   }
+	   sleep(2);
+	   Actions ac1 = new Actions(driver);
+       ac1.moveToElement(driver.findElement(By.cssSelector("button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-icon-only.ui-dialog-titlebar-close"))).build().perform();
+       driver.findElement(By.cssSelector("button.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-icon-only.ui-dialog-titlebar-close")).click();
+	    	
+     /*==========Page Navigation ======== 
+	   String s = driver.findElement(By.id("example_info")).getText();
+	   System.out.println(s);
+       driver.findElement(By.id("example_next")).click();
+       String s1 = driver.findElement(By.id("example_info")).getText();
+       System.out.println(s1);
+       if(!s1.equalsIgnoreCase(s)){
+    	   System.out.println("The page is navigated to next page successfully");
+       }
+       driver.findElement(By.id("example_previous")).click();
+       String s2 = driver.findElement(By.id("example_info")).getText();
+       System.out.println(s2);
+       if(!s1.equalsIgnoreCase(s2)){
+    	   System.out.println("The page is navigated to previous page successfully");
+       }
+  
+  
+ }*/
+  
   	
   }
 	   
