bad3fd76b3723f6d3876690f3590ca94902f7d9b
hardikpatel40 <hardikpat2011@gmail.com>
2014-10-16 09:48:07 +0530

    login update

--- src/crm/Login.java
+++ src/crm/Login.java
@@ -1,22 +1,14 @@
 package crm;
-
-import java.io.File;
 import java.util.List;
-
-import jxl.Sheet;
-import jxl.Workbook;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
+import testUtils.Helper;
 
 
-public class Login {
-	//Instantiating WebDriver
-	public WebDriver driver;
+public class Login extends Helper{
   @Test
   public void Login_CRM() throws Exception{
 	  /*
@@ -33,21 +25,21 @@ public class Login {
 	   * Getting username and password from spreadsheet 1 and trying to log in
 	   */
 	  System.out.println("Trying to login with different username and passwords from excel sheets");
-	  File fp = new File(System.getProperty("user.dir") +"\\src\\testData\\TestData.xls");
-	  Workbook wb = Workbook.getWorkbook(fp);
-	  Sheet sh = wb.getSheet(0);
-	  int rows = sh.getRows();
+	  //File fp = new File(System.getProperty("user.dir") +"\\src\\testData\\TestData.xls");
+	  //Workbook wb = Workbook.getWorkbook(fp);
+	  //Sheet sh = wb.getSheet(0);
+	  int rows = sh0.getRows();
 	  for(int i=0;i<rows;i++){
 		  	int j=0;
 		  			
-			  driver.findElement(By.id("username")).sendKeys(sh.getCell(j, i).getContents());
+			  driver.findElement(By.id("username")).sendKeys(sh0.getCell(j, i).getContents());
 			  j++;
-			  driver.findElement(By.id("password")).sendKeys(sh.getCell(j, i).getContents());
+			  driver.findElement(By.id("password")).sendKeys(sh0.getCell(j, i).getContents());
 			  driver.findElement(By.cssSelector("p.login.button")).findElement(By.tagName("input")).click();
 			  driver.findElement(By.cssSelector("p.login.button")).findElement(By.tagName("input")).click();
-			  Thread.sleep(2000);
+			  sleep(2);
 			  List<WebElement> error_msg = driver.findElement(By.id("wrapper")).findElements(By.tagName("label"));
-			  System.out.println("logging with username: " +sh.getCell(j-1, i).getContents() +", "+"password: " +sh.getCell(j, i).getContents() +":::" +error_msg.get(2).getText());
+			  System.out.println("logging with username: " +sh0.getCell(j-1, i).getContents() +", "+"password: " +sh0.getCell(j, i).getContents() +"-->" +error_msg.get(2).getText());
 	  						}
 	  
 	  		  //Checking for forgot password link
@@ -57,7 +49,7 @@ public class Login {
 	  		  
 	  		  //clicking forgot password link
 			  driver.findElement(By.id("wrapper")).findElement(By.tagName("a")).click();
-			  Thread.sleep(1000);
+			  sleep(1);
 			  
 			  //Checking for submit button in forgot password link
 			  if(driver.findElement(By.cssSelector("p.login.button")).findElement(By.tagName("input")).getAttribute("value").equals("Submit")){
@@ -70,7 +62,7 @@ public class Login {
 		/*
 		 * Getting usernames from spreadsheet 2 and trying to retreive password
 		*/
-			  Sheet sh1 = wb.getSheet(1);
+			  //Sheet sh1 = wb.getSheet(1);
 			  int rows1 = sh1.getRows();
 			  int j=0;
 	  for(int i=0;i<rows1;i++){
@@ -79,7 +71,7 @@ public class Login {
 		  driver.findElement(By.id("username")).sendKeys(sh1.getCell(j, i).getContents());
 		  driver.findElement(By.cssSelector("p.login.button")).findElement(By.tagName("input")).click();
 		  driver.findElement(By.cssSelector("p.login.button")).findElement(By.tagName("input")).click();
-		  Thread.sleep(6000);
+		  sleep(6);
 		  List<WebElement> error_msg = driver.findElement(By.id("wrapper")).findElements(By.tagName("label"));
 			  if(error_msg.get(3).getText().isEmpty()){
 				  System.out.print(error_msg.get(2).getText());
@@ -92,14 +84,15 @@ public class Login {
 	  }
 	  
 	  //Getting Log in credentials from spreadsheet 3 and trying to log in 
-	  Sheet sh2 = wb.getSheet(2);
+	  //Sheet sh2 = wb.getSheet(2);
 	  driver.findElement(By.id("username")).sendKeys(sh2.getCell(0, 0).getContents());
 	  driver.findElement(By.id("password")).sendKeys(sh2.getCell(1, 0).getContents());
 	  driver.findElement(By.cssSelector("p.login.button")).findElement(By.tagName("input")).click();
 	  driver.findElement(By.cssSelector("p.login.button")).findElement(By.tagName("input")).click();
-	  Thread.sleep(2000);
-	  if(driver.getTitle().equals("::NVT:: BDM Home Page")){
-		  System.out.println("Logging with username: " +sh2.getCell(0, 0).getContents()+"," +"password: " +sh2.getCell(1, 0).getContents());
+	  System.out.println("Logging with username: " +sh2.getCell(0, 0).getContents()+"," +"password: " +sh2.getCell(1, 0).getContents());
+	  sleep(3);
+	  System.out.println(driver.getTitle());
+	  if(driver.getTitle().equals("::Lead CRM:: BDM Home Page")){
 		  System.out.println("LOGGED IN SUCCESSFULLY");
 	  }else{
 		  System.out.println("Error in log in");
@@ -115,8 +108,8 @@ public class Login {
    *  Opening Firefox browser and getting the Lead CRM home page
    */
   public void beforeMethod() {
-	  driver = new FirefoxDriver();
-	 driver.get("http://192.168.50.32:8080/leadcrm/login.jsp");
+	  browser();
+	 driver.get(config.getProperty("url"));
 	 if(driver.getTitle().equals("::LEAD-CRM::Login Here")){
 		 System.out.println("Lead CRM URL found");
 	 }else{
