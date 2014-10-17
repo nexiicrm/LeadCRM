64608ae8fa58c43dc9b9df3a2700872f1c9004d1
anuhyamalla <anuhyamalla@gmail.com>
2014-10-17 19:10:21 +0530

    Reuploading BDM.java

--- src/crm/BDM.java
+++ src/crm/BDM.java
@@ -15,7 +15,6 @@ import testUtils.Helper;
 
 public class BDM extends Helper {
 	
-	
 	 @BeforeMethod
 	 public void before() throws Exception {
 		browser();
@@ -24,208 +23,165 @@ public class BDM extends Helper {
 		browsererror();
 	
 		//Login for BDM module
-		login(sh2.getCell(0,0).getContents(), sh2.getCell(1,0).getContents());
+		help.login(sh2.getCell(0,0).getContents(), sh2.getCell(1,0).getContents());
 		String user = driver.findElement(By.className("user_name")).getText();
 		System.out.println("User Logged in as:" + user);
 		
 	 }
+	 
+	 
+/*	 //Test method for Proposal follow up
+	 @Test
+	 public void proposalFollowup() {
+		 help.expand();
+		 System.out.println("Click on the '" + driver.findElement(By.id("allfollowups")).getText() + "' Link" );
+		 driver.findElement(By.id("allfollowups")).click();
+		 help.sleep(2);
+		 search("Introductory Mail");
+		 List <WebElement> leads = driver.findElement(By.id("example")).findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+		 if(leads.size()==0)
+			 System.out.println("No leads present to Prospect Identify for Proposals.");
+		 else { 
+			 leads.get(0).findElement(By.className("work")).click();
+			 
+		 }
+		 
+	 }*/
 	
 	 
 	 //Test method for Proposal upload
 	 @Test
-	 public void proposal() throws InterruptedException {
+	 public void proposalUpload() {
 		  
-		  //Verifying the no.of options in the left pane and Expanding the Proposal Uploads option
-		  List <WebElement> options = driver.findElement(By.id("tree_menu")).findElements(By.className(" close"));
-		  System.out.println("The left pane has '" + options.size() + "' Options");
-		  System.out.println("The option selected is:" + options.get(4).getText());
-		  options.get(4).findElement(By.tagName("span")).click();
+		  help.expand();
 		  System.out.println("Click on the '" + driver.findElement(By.id("proposalupload")).getText() + "' Link" );
 		  driver.findElement(By.id("proposalupload")).click();
 		  
 		  //Verifying whether the required page is loaded or not
 		  System.out.println("Page loaded is:" + driver.findElement(By.id("container")).findElement(By.tagName("h1")).getText());
+		  help.sleep(4);
 		  
 		  if(driver.findElement(By.id("example_info")).getText().equals("Showing 0 to 0 of 0 entries"))
 			  System.out.println(driver.findElement(By.className("dataTables_empty")).getText());
-		  
 		  else {
-			  //Verifying no.of leads in the page
-			  List <WebElement> leads = driver.findElement(By.id("example")).findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
-			  System.out.println("No. of leads in the Lead Edit Table:" + leads.size());
-			  
-			  //Checking for the Upload button for each lead
-			  int upload=0;
-			  for(int i=0; i<leads.size(); i++) {
-				if(leads.get(i).findElement(By.className("upload")).isEnabled()) {
-				  upload++;
-				}	  
-			  }
-			  if(upload==leads.size())
-				  System.out.println("Upload button is enabled for all " + upload + " leads.");
-		  
-			  //Clicking on Upload button of a Lead opens a Quote Upload Page
-			  Random r = new Random();
-			  int opt = r.nextInt(leads.size());
-			  leads.get(opt).findElement(By.className("upload")).click();
-			  sleep(5);
-			  System.out.println(driver.findElement(By.cssSelector("span.ui-dialog-title")).getText());
-			  
+			   uploadButton();
 			  //Entering details to the fields in Quote Upload page
 			  driver.findElement(By.name("proposalname")).sendKeys("Proposal Name");
 			  driver.findElement(By.id("button")).click();
 			  driver.findElement(By.name("proposaldescription")).sendKeys("Proposal Description");
 			  driver.findElement(By.id("button")).click();
 			  driver.findElement(By.name("proposal")).sendKeys("E:\\abc1.txt");
-			  //driver.findElement(By.id("button")).click();
-			  Thread.sleep(4000);
+			  driver.findElement(By.id("button")).click();
+			  help.sleep(4);
 			  //Verifying the Success message displayed on the page after uploading the Proposal
-			  //System.out.println(driver.findElement(By.id("result_msg_div")).findElement(By.className("success_msg")).getText());
+			  System.out.println(driver.findElement(By.id("result_msg_div")).findElement(By.className("success_msg")).getText());
 			  
 			  //Closing the Proposal Upload page
 			  driver.findElement(By.cssSelector("span.ui-button-icon-primary.ui-icon.ui-icon-closethick")).click();
-			  
-			  //Collapsing Proposal Uploads option
-			  driver.findElement(By.id("tree_menu")).findElement(By.className(" open")).findElement(By.className("  symbol-open")).click();
-			  
-			  
+			  help.collapse();  
 		  }//End of else condition
-	 }
+	 } 
 	 
 	 @Test
-	 public void quote() throws InterruptedException {
-		  
-		  //Verifying the no.of options in the left pane and Expanding the Quote Uploads option
-		  List <WebElement> option = driver.findElement(By.id("tree_menu")).findElements(By.className(" close"));
-		  System.out.println("The left pane has '" + option.size() + "' Options");
-		  System.out.println("The option selected is:" + option.get(5).getText());
-		  option.get(5).findElement(By.tagName("span")).click();
+	 public void quoteUpload() {
+
+		  help.expand();
 		  System.out.println("Click on the '" + driver.findElement(By.id("quoteupload")).getText() + "' Link" );
 		  driver.findElement(By.id("quoteupload")).click();
 		  
 		  //Verifying whether the required page is loaded or not
 		  System.out.println("Page loaded is:" + driver.findElement(By.id("container")).findElement(By.tagName("h1")).getText());
+		  sleep(4);
 		  
 		  if(driver.findElement(By.id("example_info")).getText().equals("Showing 0 to 0 of 0 entries"))
-			  System.out.println(driver.findElement(By.className("dataTables_empty")).getText());
-		  
+			 System.out.println(driver.findElement(By.className("dataTables_empty")).getText());
 		  else {
 			  //Verifying no.of leads in the page
-			  List <WebElement> leads = driver.findElement(By.id("example")).findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
-			  System.out.println("No. of leads in the Lead Edit Table:" + leads.size());
-			  
-			  //Checking for the upload button for each lead
-			  int upload=0;
-			  for(int i=0; i<leads.size(); i++) {
-				if(leads.get(i).findElement(By.className("upload")).isEnabled()) {
-				  upload++;
-				}	  
-			  }
-			  if(upload==leads.size()) 
-				  System.out.println("Upload button is enabled for all " + upload + " leads.");
-		  
-			  //Clicking on Upload button of a Lead opens a Quote Upload Page
-			  Random r = new Random();
-			  int opt = r.nextInt(leads.size());
-			  leads.get(opt).findElement(By.className("upload")).click();
-			  sleep(5);
-			  System.out.println(driver.findElement(By.cssSelector("span.ui-dialog-title")).getText());
-			  
+			  uploadButton();
 			  //Entering details to the fields in Quote Upload page
 			  driver.findElement(By.name("quotename")).sendKeys("Quote Name");
 			  driver.findElement(By.id("button")).click();
 			  driver.findElement(By.name("quotedescription")).sendKeys("Quote Description");
 			  driver.findElement(By.id("button")).click();
 			  driver.findElement(By.name("quote")).sendKeys("E:\\abc2.txt");
-			  
-			  //driver.findElement(By.id("button")).click();
-			  sleep(8);
+			  driver.findElement(By.id("button")).click();
+			  help.sleep(5);
 			  //Verifying the Success message displayed on the page after uploading the Proposal
-			  //System.out.println(driver.findElement(By.id("result_msg_div")).findElement(By.className("success_msg")).getText());
+			  System.out.println(driver.findElement(By.id("result_msg_div")).findElement(By.className("success_msg")).getText());
 			  
 			  //Closing the Quote Upload page
 			  driver.findElement(By.cssSelector("span.ui-button-icon-primary.ui-icon.ui-icon-closethick")).click();
-			  
-			  //Collapsing Quote Uploads option
-			  driver.findElement(By.id("tree_menu")).findElement(By.className(" open")).findElement(By.className("  symbol-open")).click();
+			  help.collapse();
 		  }
 		 
 	 } 
 	 
 	 
+	 @Test 
+	 public void testLeadTrackButton() {
+		 help.expand();
+		 //Clicking on Edit Leads link
+		 System.out.println("Click on the '" + driver.findElement(By.id("editLeads")).getText() + "' Link" );
+		 driver.findElement(By.id("editLeads")).click();
+		 help.sleep(3);
+		 List <WebElement> leads1 = driver.findElement(By.id("example")).findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+		 System.out.println("No. of leads in the Lead Edit Table:" + leads1.size());
+		 
+		  //Checking for the Track it button for each lead
+		  int trackit=0;
+		  for(int i=0; i<leads1.size(); i++) {
+			if(leads1.get(i).findElement(By.className("analyse")).isEnabled()) {
+			  
+			  trackit++;
+			}	  
+		  }
+		  if(trackit==leads1.size())
+			  System.out.println("Trackit button is enabled for all leads.");
+		  
+		  trackitButton(help.random(leads1.size()));
+	 } 
+	 
 	 @Test
-	 public void testleadEdit() throws InterruptedException {
+	 public void testLeadEditButton() {
 		  
-		  //Verifying the no.of options in the left pane and Expanding the Lead Edit option
-		  List <WebElement> options = driver.findElement(By.id("tree_menu")).findElements(By.className(" close"));
-		  System.out.println("The left pane has '" + options.size() + "' Options");		  
-		  System.out.println("The option selected is:" + options.get(8).getText());
-		  options.get(8).findElement(By.tagName("span")).click();
-		  
+		  help.expand();
 		  //Clicking on Edit Leads link
 		  System.out.println("Click on the '" + driver.findElement(By.id("editLeads")).getText() + "' Link" );
 		  driver.findElement(By.id("editLeads")).click();
 		  
 		  //Verifying whether the required page is loaded or not
 		  System.out.println("Page loaded is:" + driver.findElement(By.id("container")).findElement(By.tagName("h1")).getText());
+		  pagination();
+		  //pageEntries();
+		  driver.findElement(By.name("example_length")).findElements(By.tagName("option")).get(3).click();
 		  
-		  //Verifying Pagination for the Lead Edit page(Previous and Next buttons)
-		  System.out.println("Verifying Next & Previous Buttons:");
-		  System.out.println(driver.findElement(By.id("example_info")).getText());
-		  sleep(2);
-		  driver.findElement(By.id("example_next")).click();
-		  System.out.println("Clicked on Next button");
-		  System.out.println(driver.findElement(By.id("example_info")).getText());
-		  sleep(2);
-		  driver.findElement(By.id("example_previous")).click();
-		  System.out.println("Clicked on Previous button");
-		  System.out.println(driver.findElement(By.id("example_info")).getText());
-		  
-		  //Selecting no.of entries for the table
-		  driver.findElement(By.id("example_length")).click();
-		  List <WebElement> entries = driver.findElement(By.name("example_length")).findElements(By.tagName("option"));
-		  Thread.sleep(4000);
-		  System.out.println(entries.size());
-		   Random r = new Random();
-		  int opt = r.nextInt(entries.size());
-		  entries.get(opt).click();
-		  System.out.println("No.of Entries selected for the page:" + entries.get(opt).getText());
 		  
 		  //Verifying no.of leads in the page
 		  List <WebElement> leads = driver.findElement(By.id("example")).findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+		  help.sleep(3);
 		  System.out.println("No. of leads in the Lead Edit Table:" + leads.size());
-		 
-		  //Checking for the Track it and Edit buttons for each lead
-		  int edit=0, trackit=0;
+		  
+		  //Checking for the buttons for each lead
+		  int edit=0;
 		  for(int i=0; i<leads.size(); i++) {
-			if((leads.get(i).findElement(By.className("analyse")).isEnabled()) && (leads.get(i).findElement(By.className("edit")).isEnabled())) {
+			if(leads.get(i).findElement(By.className("edit")).isEnabled()) {
 			  edit++;
-			  trackit++;
 			}	  
 		  }
-		  if((edit==leads.size()) && (trackit==leads.size()))
-			  System.out.println("Trackit and Edit buttons are enabled for all leads.");
+		  if(edit==leads.size())
+			  System.out.println("Edit buttons are enabled for all leads.");
 		  
 		  //Validating the Track it button
-		  Random r1 = new Random();
-		  int opt1 = r1.nextInt(leads.size());
-		  leads.get(opt1).findElement(By.className("analyse")).click();
-		  sleep(3);
-		  
-		  //Printing the details of the table
-		  String details = driver.findElement(By.tagName("table")).findElement(By.tagName("tbody")).getText();
-		  System.out.println("____________________________________________________");
-		  System.out.println(details);
-		  System.out.println("____________________________________________________");
+		  int opt1 = help.random(leads.size());
+		  String detail =  trackitButton(opt1);
 		  
 		  //Editing the Leads
 		  driver.findElement(By.id("editLeads")).click();
 		  driver.findElement(By.name("example_length")).findElements(By.tagName("option")).get(3).click();
-		  sleep(5);
+		  help.sleep(5);
 		  driver.findElement(By.id("example")).findElement(By.tagName("tbody")).findElements(By.tagName("tr")).get(opt1).findElement(By.className("edit")).click();
-		  sleep(5);
+		  help.sleep(5);
 		  System.out.println(driver.findElement(By.cssSelector("span.ui-dialog-title")).getText());
-		  
 		  driver.findElement(By.id("firstname")).clear();
 		  driver.findElement(By.id("firstname")).sendKeys("Jennifer");
 		  driver.findElement(By.id("lastname")).clear();
@@ -239,27 +195,90 @@ public class BDM extends Helper {
 		  driver.findElement(By.id("desknumber")).clear();
 		  driver.findElement(By.id("desknumber")).sendKeys("3-(618)434-8752");
 		  driver.findElement(By.id("editbutton")).click();
-		  sleep(3);
+		  help.sleep(3);
 		  System.out.println(driver.findElement(By.id("result_msg_div")).findElement(By.className("success_msg")).getText());
 		  driver.findElement(By.cssSelector("span.ui-button-icon-primary.ui-icon.ui-icon-closethick")).click();
 		  
-		  
-		  
 		  //Tracking the edited lead and compare for modifications
 		  driver.findElement(By.name("example_length")).findElements(By.tagName("option")).get(3).click();
-		  sleep(5);
-		  driver.findElement(By.id("example")).findElement(By.tagName("tbody")).findElements(By.tagName("tr")).get(opt1).findElement(By.className("analyse")).click();
-		  sleep(3);
-		  String detailsModified = driver.findElement(By.tagName("table")).findElement(By.tagName("tbody")).getText();
-		  System.out.println("____________________________________________________");
-		  System.out.println(detailsModified);
-		  System.out.println("____________________________________________________");
-		  
-		  if(details.equals(detailsModified))
+		  help.sleep(5);
+		  String detail1 = trackitButton(opt1);
+		  if(detail.equals(detail1))
 			  System.out.println("The Lead is not edited");
 		  else {
 			  System.out.println("The lead details are modified.");
 		  }  
-	 }
+	 } 
+	 
+	 
+	// Static Methods 
+		
+		 //Method for validating Upload button
+		 public static void uploadButton() {
+			  //Verifying no.of leads in the page
+			  List <WebElement> leads = driver.findElement(By.id("example")).findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+			  System.out.println("No. of leads in the Table:" + leads.size());
+			  int upload=0;
+			  for(int i=0; i<leads.size(); i++) {
+				if(leads.get(i).findElement(By.className("upload")).isEnabled()) {
+				  upload++;
+				}	  
+			  }
+			  if(upload==leads.size())
+				  System.out.println("Upload button is enabled for all " + upload + " leads.");
+			  //Clicking on Upload button of a Lead opens a Quote Upload Page
+			  int opt = help.random(leads.size());
+			  leads.get(opt).findElement(By.className("upload")).click();
+			  help.sleep(5);
+			  System.out.println(driver.findElement(By.cssSelector("span.ui-dialog-title")).getText());
+		 }
+		 
+		 
+		 //No. of Entries per page
+		 public  void pageEntries() {
+			  //Selecting no.of entries for the table
+			  driver.findElement(By.id("example_length")).click();
+			  List <WebElement> entries = driver.findElement(By.name("example_length")).findElements(By.tagName("option"));
+			  help.sleep(4000);
+			  System.out.println(entries.size());
+			  int opt = help.random(entries.size());
+			  entries.get(opt).click();
+			  System.out.println("No.of Entries selected for the page:" + entries.get(opt).getText());
+		 }
+		 
+		 //Method for Pagination(Previous and Next buttons)
+		 public static void pagination() {
+			  System.out.println("Verifying Next & Previous Buttons:");
+			  System.out.println(driver.findElement(By.id("example_info")).getText());
+			  help.sleep(2);
+			  driver.findElement(By.id("example_next")).click();
+			  System.out.println("Clicked on Next button");
+			  System.out.println(driver.findElement(By.id("example_info")).getText());
+			  help.sleep(2);
+			  driver.findElement(By.id("example_previous")).click();
+			  System.out.println("Clicked on Previous button");
+			  System.out.println(driver.findElement(By.id("example_info")).getText());
+		 }
+
+		 
+		 //Method for validating Track It button
+		 public static String trackitButton(int opt1) {
+			  List <WebElement> leads1 = driver.findElement(By.id("example")).findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+			  leads1.get(opt1).findElement(By.className("analyse")).click();
+			  help.sleep(3);
+			  //Printing the details of the table
+			  String details = driver.findElement(By.tagName("table")).findElement(By.tagName("tbody")).getText();
+			  System.out.println("____________________________________________________");
+			  System.out.println(details);
+			  System.out.println("____________________________________________________");
+			  return details; 
+		 }
+		 
+		 
+		 //Method for validating Search box
+		 public static void search(String keyword) {
+			 driver.findElement(By.id("example_filter")).findElement(By.tagName("input")).sendKeys(keyword);
+			 
+		 }
 
 }
