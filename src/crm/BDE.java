ef59cc4f3f2c960c4d037babd68ae126e161c07b
vlakshmi90 <lakshmipriya.sure@gmail.com>
2014-10-17 19:38:28 +0530

    "Upload of BDE on Friday"

--- src/crm/BDE.java
+++ src/crm/BDE.java
@@ -1,21 +1,28 @@
 package crm;
 
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.Select;
 
 import testUtils.Helper;
 
 public class BDE extends Helper{
-	String[] expected = {"Research Phase", "Work ", "Closed Phase", "Cold Storage", "Lead Search", "Lead Edit", "My Account", 
+	String[] expected = {"Research Phase", "Work Phase", "Closed Phase", "Cold Storage", "Lead Search", "Lead Edit", "My Account", 
 			"Research On Company", "Work on Lead", "Today's FollowUp", "All FollowUps", "Lead Close", "Cold Storage", "Search Leads", "Edit Leads", "Change Password"};
-			
+
 	List<String> lisub = new ArrayList<String>();
 	@BeforeTest
 	public void beforeMethod() throws Exception{
@@ -25,32 +32,47 @@ public class BDE extends Helper{
 		login("sreekar.jakkula@nexiilabs.com", "password");
 		browsererror();		
 	}
-	/*@Test
+	@AfterTest
+	public void afterMethod() throws Exception{
+		driver.close();
+	}
+	@Test
 	public void ExpandCollapse()
 	{	
 		List<WebElement> tree = driver.findElement(By.id("tree_menu")).findElements(By.className("close"));
 		sleep(1);
+		System.out.println(" ++++++++ Adding Links of tree in to array lisub ++++++++++");
 		for (int i = 0; i < tree.size(); i++)
 			lisub.add(tree.get(i).getText());	
 		expand();
+		System.out.println(" ++++++++ Adding Links of tree in to array lisub ++++++++++");
 		List<WebElement> subtree= driver.findElement(By.id("tree_menu")).findElements(By.tagName("a"));
 		for (int i = 0; i < subtree.size(); i++)
 			lisub.add(subtree.get(i).getText());
 		collapse();			
+		
 		System.out.println("+++++++++ This List of Links in tree and sub tree ++++++\n" + lisub);
-	    for ( int k=0 ; k< expected.length; k++)
+		for ( int k=0 ; k< expected.length; k++)
 	    {
 	    	String list = lisub.get(k);
-	    	if (list.equals(expected[k]))
-	    		System.out.println("Passed on search of link of tree :" +list);
-	    	else
-	    		System.out.println("Failed on search of links of tree :" +list);
+	    		if (list.equals(expected[k]))
+	    			System.out.println("Passed on search of link of tree :" +expected[k]);
+	    		else 
+	    			System.out.println("Failed on search of link of tree :" +expected[k]);
 		}
-	    
-	}*/
-	@Test
-	public void ResearchOnCompany()
+	}
+
+	public void submitMessage(WebElement sb, String msg)
 	{
+	 sb.submit();
+	  List<WebElement> ermg =driver.findElement(By.id("result_msg_div")).findElements(By.tagName("label"));
+	  if(ermg.get(0).getText().equalsIgnoreCase(msg))
+		  System.out.println("Error message is displayed as expected");
+	}
+
+	String Leadno;
+	@Test
+	  public void ResearchOnCompany(){
 		expand();
 		List<WebElement> subtree= driver.findElement(By.id("tree_menu")).findElements(By.tagName("a"));
 		for (int i=0; i<subtree.size(); i++)
@@ -59,53 +81,142 @@ public class BDE extends Helper{
 			   subtree.get(i).click();
 			   System.out.println(" Navigate to 'Research On Company' page  ");
 			   sleep(2);
+			  
 			   WebElement cont= driver.findElement(By.id("container"));
 			   if (cont.findElement(By.tagName("h1")).getText().equalsIgnoreCase("Lead Research"))
 			   {
-				   System.out.println("######### The Lead Research page is navigated sucessfully. ##########");
-				   
+				   System.out.println("######### Navigated to Lead Research page. ##########");   
 				   List<WebElement> table = driver.findElement(By.id("example")).findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
 				   System.out.println(table.size() + " is " + table);
-				   WebElement res = table.get(0).findElement(By.className("segregate"));
+				   
+				   WebElement res = table.get(random(table.size())).findElement(By.className("segregate"));
+				   Leadno =res.getAttribute("id");
+				   System.out.println("Lead no is " + Leadno);
 				   res.click();
-				   //System.out.println("The Button clicked is:" +res.getText());
+				  
+				   
+				   System.out.println("######### Navigated to  ResearchOnCompany Form ##########");   
 				   WebElement cont1= driver.findElement(By.id("dialog-form"));
 				   System.out.println(cont1.findElement(By.tagName("h1")).getText());
 				   if (driver.findElement(By.tagName("h1")).getText().equalsIgnoreCase("Research on Lead"))
 				   {
 					  WebElement seg = driver.findElement(By.id("segregatebutton"));
-					  seg.submit();
-					  List<WebElement> ermg =driver.findElement(By.id("result_msg_div")).findElements(By.tagName("label"));
-					  if(ermg.get(0).getText().equalsIgnoreCase("Please Select Company Fund Status"))
-						  System.out.println("Error message is displayed as expected");
-				      
-					  WebElement lab1 = driver.findElement(By.name("companyfundstatus"));
-					  lab1.click();
-					  sleep(1);
-					  List<WebElement> l1opt= lab1.findElements(By.tagName("option"));
-					  l1opt.get(random(l1opt.size())).click();
+					  submitMessage(seg, "Please Select Company Fund Status");
+					  new Select(driver.findElement(By.name("companyfundstatus"))).selectByVisibleText("Seed Funded");
 					  sleep(1);
 					  
-					  WebElement lab2 = driver.findElement(By.name("companystatus"));
-					  lab2.click();
-					  sleep(1);
-					  List<WebElement> l2opt= lab1.findElements(By.tagName("option"));				  
-					  l2opt.get(random(l2opt.size())).click();
-					  sleep(1);
-					  
+					  submitMessage(seg,"Please Select Comapany Status");
+					  new Select(driver.findElement(By.name("companystatus"))).selectByVisibleText("Stable Growth");
+
+					  submitMessage(seg,"Please Leave a comment");
 					  WebElement cmt = driver.findElement(By.name("researchcomment"));
 					  cmt.sendKeys("This is completed as per the schedule.");
-					  
-					  
-					  
+					  sleep(1);
+					 
+					  submitMessage(seg,"Segregated Successfully and Lead Moved to Work Phase");			  
+					  //driver.navigate().back();
+					  WebElement close =  driver.findElement(By.tagName("button"));
+					  close.click();
+				   }	 
+				
+			   }	
+			}
+		}
+    public void searchLead()
+	{
+		WebElement search = driver.findElement(By.id("example_filter")).findElement(By.tagName("input"));
+		search.sendKeys(Leadno);
+		List<WebElement> table = driver.findElement(By.id("example")).findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+		System.out.println("Should display only one item,The table size is :::: " + table.size());
+		String Leadres = table.get(0).findElement(By.className("sorting_1")).getText();
+		
+	    if(Leadres.contentEquals(Leadno)){
+	    	System.out.println("The Lead is present");       
+	    }else
+	    	System.out.println("Lead not present");
+	}
+	
+	public void workPhasePage()
+	{
+		expand();
+		List<WebElement> subtree= driver.findElement(By.id("tree_menu")).findElements(By.tagName("a"));
+		for (int i=0; i<subtree.size(); i++){
+			if((subtree.get(i).getText().equalsIgnoreCase("Work on Lead")))
+			{   	   
+			   subtree.get(i).click();
+			   System.out.println(" Navigate to 'Work on Lead' page  ");
+			   sleep(2);		  
+			   WebElement cont= driver.findElement(By.id("container"));
+			   if (cont.findElement(By.tagName("h1")).getText().equalsIgnoreCase("Work on Lead"))
+			   {
+				   System.out.println("######### Navigation to Work on Lead page. ##########"); 
+				   //searchLead();
+				   List<WebElement> table = driver.findElement(By.id("example")).findElement(By.tagName("tbody")).findElements(By.tagName("tr"));
+				   System.out.println(table.size() + " is " + table);
+				   if (table.size() == 0){
+					   Assert.fail("Work Phase table field is Empty...");
 				   }
+				   WebElement res = table.get(random(table.size())).findElement(By.className("work"));
+				   res.click();
 				   
-				   
+				   System.out.println("######### Navigated to  Work on Lead Form ##########");   
+				   WebElement cont1= driver.findElement(By.id("dialog-form"));
+				   System.out.println(cont1.findElement(By.tagName("h1")).getText());
 			   }
-		   }
- 
-	}		   
-    }
+			}
+		}
+	}
+	public void workPhaseForm(String s1, String s2, String s3)
+	{
+		 WebElement seg = driver.findElement(By.id("button"));
+		  submitMessage(seg, "Please Select Followup Type");
+		  new Select(driver.findElement(By.name("followuptype"))).selectByVisibleText(s1);
+		  sleep(1);
+	 				 
+		  submitMessage(seg,"Please select Followup Type");
+		  WebElement cmt = driver.findElement(By.name("followupcomment"));
+		  cmt.sendKeys(s2);
+		  sleep(1);
+		  
+		  submitMessage(seg,"Please Enter next Follow up date");			  	  					 
+		  WebElement folldate = driver.findElement(By.id("nextfollowupdate"));
+		  folldate.sendKeys(s3);
+		  sleep(1);
+		  folldate.click();
+	
+		  submitMessage(seg,"Successfully Updated."); 
+		 
+	}
+		  @Test
+		  public void workPhase1()
+		  {
+			Date date = new Date();
+			SimpleDateFormat simple = new SimpleDateFormat("yyyy-M-dd");
+			Calendar cal = Calendar.getInstance();
+			cal.add(Calendar.DAY_OF_YEAR, 2);
+			Date later = cal.getTime();
+			
+		    workPhasePage();
+		    if (driver.findElement(By.tagName("h1")).getText().equalsIgnoreCase("Work on Lead"))
+		    {		    	
+				workPhaseForm("Introductory Mail","selection today date",simple.format(date));
+			    WebElement close =  driver.findElement(By.tagName("button"));
+				close.click();	 
+		    }
+		    /*workPhasePage();
+		    if (driver.findElement(By.tagName("h1")).getText().equalsIgnoreCase("Work on Lead"))
+		    {
+				workPhaseForm("Introductory Mail","selection today date",simple.format(later));
+				WebElement close =  driver.findElement(By.tagName("button"));
+				close.click();	
+				
+			}*/	
+		 }
+	     
+		  
+	  }
+
+
 	
 			   
     
